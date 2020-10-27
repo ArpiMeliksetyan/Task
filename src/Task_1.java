d2ebf3acbb106807a672d8cb3af060c5835f5090
Arpi <a.meliqsetyan@inbox.ru>
2020-10-27 14:58:45 +0400

    1

---
+++ src/Task_1.java
@@ -0,0 +1,44 @@
+import java.sql.SQLOutput;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+        int x = 10000;
+        System.out.println(x);
+
+        short y = 7050;
+        System.out.println(y);
+
+        byte z = 105;
+        System.out.println(z);
+
+        char c = 'A';
+        System.out.println(c);
+
+        char c1 = '\u0108';
+        System.out.println(c1);
+
+        float f = 177.7F;
+        System.out.println(f);
+
+        double d = 125.7;
+        System.out.println(d);
+
+        long l = 10000000L;
+        System.out.println(l);
+
+        boolean t = true;
+        System.out.println(t);
+
+        boolean t1 = false;
+        System.out.println(t1);
+
+        String s = "Hello world !!!";
+        System.out.println(s);
+
+        s = s + " " + "Armenia";
+        System.out.println(s);
+
+
+    }
+}
